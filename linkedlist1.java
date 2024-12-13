d3b97010fe0263e4e7822d7bfa0b1bb0b02c57e3
Mahamood <shaikmisba305@gmail.com>
2024-12-13 10:20:45 +0530

    Add files via upload

---
+++ linkedlist1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        // accepting values into  a linkedlist
+        LinkedList<Integer> a=new LinkedList<>();
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        for(int i=0;i<size;i++)
+        {
+            int s=sc.nextInt();
+            a.addFirst(s);
+        }
+        for(int i=0;i<size;i++)
+        System.out.println(a.get(i)+" ");
+    }
+}
